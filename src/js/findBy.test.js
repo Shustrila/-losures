a41f10d682488753abc0c6912df7a1d19c83a484
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-02-07 00:37:52 +0300

    v1: edit

--- src/js/findBy.test.js
+++ src/js/findBy.test.js
@@ -1,18 +1,51 @@
 import findBy from "./findBy.js";
 
-test("array the search and result function findBy",() => {
-	const finder = findBy('name', 'урон');
-	const results = [
-		{name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
-		{name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
-		{name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
-	].filter(finder);
+describe("TEST: find by", () =>{
 
-	let arrResults = [{
-		name: 'урон',
-		type: 'help',
-		description: 'Страница описания элемента интерфейса'
-	}];
+	test("search to key name and value damage",() => {
+		const finder = findBy('name', 'урон');
+		const results = [
+			{name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
+			{name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
+			{name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
+		].filter(finder);
 
-	expect(results).toEqual(arrResults);
+		let arrResults = [{
+			name: 'урон',
+			type: 'help',
+			description: 'Страница описания элемента интерфейса'
+		}];
+
+		expect(results).toEqual(arrResults);
+	});
+
+	test("search to key type and attack",() => {
+		const finder = findBy('type', 'attack');
+		const results = [
+			{name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
+			{name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
+			{name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
+		].filter(finder);
+
+		let arrResults = [{
+			name: 'заклинание',
+			type: 'attack',
+			description: 'Атака магическим заклинанием'
+		}];
+
+		expect(results).toEqual(arrResults);
+	});
+
+
+	test("forgot about everything and gave nothing",() => {
+		const finder = findBy('', '');
+		const results = [
+			{name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
+			{name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
+			{name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
+		].filter(finder);
+
+		expect(results).toEqual([]);
+	});
+
 });
